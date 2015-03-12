c4a3968fd15c6c9c2f687e58dcd6617acc1670da
shanecalder <shane.calder@impartior.com>
2015-03-11 23:19:09 -0600

    Committed by Udacity

---
+++ Pitch Perfect Project/Pitch Perfect Project/RecordedAudio.swift
@@ -0,0 +1,14 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect Project
+//
+//  Created by shane calder on 2015-03-07.
+//  Copyright (c) 2015 Shane Calder. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+}
